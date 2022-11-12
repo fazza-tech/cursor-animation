8a60a8dbaf96d9e9f320dcd7eae3a94b1b97c9af
Fasal Muhammed <98795366+savvycoder-tech@users.noreply.github.com>
2022-11-12 15:52:36 +0530

    nav

--- app.js
+++ app.js
@@ -7,3 +7,14 @@ function cursor(e){
     mouseCursor.style.top = e.pageY + 'px';
     mouseCursor.style.left = e.pageX + 'px';
 }
+
+navLinks.forEach(Link => {
+    Link.addEventListener('mouseleave',() => {
+        mouseCursor.classList.remove("link-grow");
+        Link.classList.remove("hovered-link");
+    });
+    Link.addEventListener('mouseover',() => {
+        mouseCursor.classList.add("link-grow");
+        Link.classList.add("hovered-link");
+    });
+});
